787c6fac8f984c59b91bc6d8f9968db70b0c7e4f
Icadamus <richardamus06@gmail.com>
2023-08-24 00:15:01 +0800

    foto

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -7,7 +7,7 @@
                 <div class="avatar">
                     @if(!empty(Auth::guard('pegawai')->user()->foto))
                     @php
-                        $path = Storage::url('uploads/pegawai/'.Auth::guard('pegawai')->user()->foto);
+                        $path = Storage::url('public/uploads/pegawai/'.Auth::guard('pegawai')->user()->foto);
                     @endphp
                     <img src="{{url($path)}}" alt="avatar" class="imaged w64 rounded">
                     @else
@@ -79,7 +79,7 @@
                                     <div class="iconpresence">
                                         @if ($presensihariini !== null)
                                         @php
-	                                    $path= Storage::url('uploads/absensi/'.$presensihariini->foto_in);
+	                                    $path= Storage::url('public/uploads/absensi/'.$presensihariini->foto_in);
                                         @endphp
                                         <img src="{{url($path)}}"alt="" class="imaged w48">
                                         @else
@@ -101,7 +101,7 @@
                                     <div class="iconpresence">
                                         @if ($presensihariini !== null && $presensihariini->jam_keluar !== null)
                                         @php
-	                                    $path= Storage::url('uploads/absensi/'.$presensihariini->foto_out);
+	                                    $path= Storage::url('public/uploads/absensi/'.$presensihariini->foto_out);
                                         @endphp
                                         <img src="{{url($path)}}"alt="" class="imaged w48">
                                         @else
@@ -189,7 +189,7 @@
                         <ul class="listview image-listview">
                             @foreach ($historibulanini as $d)
                             @php
-	                            $path= Storage::url('uploads/absensi/'.$d->foto_in)
+	                            $path= Storage::url('public/uploads/absensi/'.$d->foto_in)
                             @endphp
                             <li>
                                 <div class="item">
